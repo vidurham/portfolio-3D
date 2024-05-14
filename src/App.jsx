1012c5244ce714163dab43a87462e9fba3db4402
vidurham <vdurham@prideseals.com>
2024-05-14 15:29:10 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Home, About, Projects, Contact} from './pages';
 
