5dacddd8a6a17b9aa44855c71e2290a21240be07
vidurham <vdurham@prideseals.com>
2024-05-14 14:54:52 -0400

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base: 'https://vidurham.github.io/portfolio-3D/',
 })
